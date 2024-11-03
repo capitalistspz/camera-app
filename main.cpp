022415fa8b5d0add628bf2fcf40081ae7f00a3ee
capitalistspz <keipitalists@proton.me>
2024-11-03 18:10:50 +0000

    Fixed NV12 saving

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include "gfx.hpp"
 #include <CafeGLSLCompiler.h>
 #include <cerrno>
+#include <chrono>
 #include <cstdio>
 #include <cstring>
+#include <format>
 #include <sysapp/launch.h>
 #include <vpad/input.h>
 #include <whb/gfx.h>
@@ -12,22 +14,9 @@
 #include <whb/log_udp.h>
 #include <whb/proc.h>
 
-void DeInit()
-{
-    WHBGfxShutdown();
-    WHBLogCafeDeinit();
-    WHBLogUdpDeinit();
-    WHBProcShutdown();
-}
-
-void Exit()
-{
-    SYSLaunchMenu();
-    while (WHBProcIsRunning())
-    {
-    }
-    // GLSL_Shutdown();
-}
+void DeInit();
+void Exit();
+void SaveNV12(const uint8_t* buf);
 
 int main()
 {
@@ -60,28 +49,13 @@ int main()
 
     while (WHBProcIsRunning())
     {
-        auto buf = camera::GetSurfaceBuffer();
+        auto* buf = camera::GetSurfaceBuffer();
         if (const auto count = VPADRead(VPAD_CHAN_0, &vpadStatus, 1, &vpadReadError); count > 0)
         {
             const auto justChanged = vpadStatus.hold & ~prevVpadStatus.hold;
             if (justChanged & VPAD_BUTTON_A)
             {
-                WHBLogPrint("Starting image capture");
-                auto* const file = std::fopen("/vol/external01/wiiu/cam/img4.nv12", "wb");
-                if (!file)
-                {
-                    WHBLogPrintf("Failed to open file: %s", std::strerror(errno));
-                    continue;
-                }
-                const auto bytesWritten = std::fwrite(buf, 1, CAMERA_YUV_BUFFER_SIZE, file);
-                WHBLogPrintf("Write %d bytes", bytesWritten);
-                if (std::fclose(file) != 0)
-                {
-                    WHBLogPrintf("Failed to close file: %s", std::strerror(errno));
-                    continue;
-                }
-
-                WHBLogPrint("Successfully saved image");
+                SaveNV12(static_cast<uint8_t*>(buf));
             }
             prevVpadStatus = vpadStatus;
         }
@@ -92,4 +66,44 @@ int main()
     camera::Exit();
     DeInit();
     return 0;
+}
+
+void DeInit()
+{
+    WHBGfxShutdown();
+    WHBLogCafeDeinit();
+    WHBLogUdpDeinit();
+    WHBProcShutdown();
+}
+
+void Exit()
+{
+    SYSLaunchMenu();
+    while (WHBProcIsRunning())
+    {
+    }
+    DeInit();
+}
+void SaveNV12(const uint8_t* buf)
+{
+    WHBLogPrint("Saving a photo");
+    namespace chr = std::chrono;
+    const auto localNow = chr::current_zone()->to_local(chr::system_clock::now());
+    auto path = std::format("/vol/external01/wiiu/cam/IMG-{0:%F}-{0:%H}-{0:%M}-{0:%S}.nv12",
+                            chr::floor<chr::milliseconds>(localNow));
+    auto* const file = std::fopen(path.c_str(), "wb");
+    if (!file)
+    {
+        WHBLogPrintf("Failed to open file: %s", std::strerror(errno));
+        return;
+    }
+    for (auto line = 0u; line < (CAMERA_HEIGHT + (CAMERA_HEIGHT >> 1)); ++line)
+    {
+        std::fwrite(buf + CAMERA_PITCH * line, 1, CAMERA_WIDTH, file);
+    }
+
+    if (std::fclose(file) != 0)
+        WHBLogPrintf("Failed to close file: %s", std::strerror(errno));
+    else
+        WHBLogPrint("Successfully saved photo");
 }
